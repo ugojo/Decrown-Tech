69a813902aee798c7bf85c5892c1d65451833654
unknown <you@example.com>
2020-06-08 01:28:57 -0700

    update

--- js/script.js
+++ js/script.js
@@ -7,12 +7,31 @@
   // });  
 //});
 
-   $('#meun a').on( 'click' ,function (e) {
-       e.preventDefult();
-       var url = this.href ;
-       $('meun a.current').reoveClass();
-       $(this).addClass('current');
+   var xhr = XMLHttpRequest() ;
+   xhr.onload = function () { 
+     if (xhr.status==200) {
+       document.getElementById ('warpper').innerHTML = xhr.responseText;
+     }
+   };
+   xhr.open ( 'GET' , 'data/data.html', true);
+   xhr.send(null);
 
-       $('section').remove();
-       $('#warpper').load(url + '#warpper').hide().fadeIn('slow');
+   $('ul a').on( 'click' ,function (e) {
+    e.preventDefault();
+   var url = this.href ;
+
+    $('ul a.current').removeClass();
+    $(this).addClass('current');
+    $('article').remove();
+    $('#warpper').load( url  + ' #warpper').hide().fadeIn('slow');
+});
+
+$( function () {
+   $('#form').on('submit' ,function (e) {
+      e.preventDefault() ;
+   var details = $('#form').serialize() ;
+   $.post ( 'https://ugojo.github.io/Decrown-tech/' , details, function (data) {
+      $('#form').html(data)
    });
+   });
+});
