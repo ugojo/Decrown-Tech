f768d0306828eecb56d8e6b7ff029da9cc163a9e
ugojo <obiugochukwu003@gmail.com>
2020-11-17 07:21:01 -0800

    update

--- js/portfoilosTabScript.js
+++ js/portfoilosTabScript.js
@@ -82,3 +82,27 @@ function myAutoSlide() {
  autodots[slideIndexs - 1].className +=  " activeslide";
  setTimeout( myAutoSlide, 3000);
 }
+/* Accordion */
+var btn ,i ,panel ;
+
+btn = document.getElementsByClassName("accordion");
+for ( i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click" , function() {
+        
+        this.classList.toggle("activeAcc");
+        panel = this.nextElementSibling ;
+
+        if (panel.style.display === "block") {
+
+            panel.style.display = "none";
+        }else{
+            panel.style.display = "block";
+        }
+        if (panel.style.maxHeight) {
+
+            panel.style.maxHeight = "null";          
+        }else{
+            panel.style.maxHeight = panel.scrollHeight + "px" ;
+        }
+    })
+}
