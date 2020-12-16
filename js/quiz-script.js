a4e525ac3c66c931e0a05ef2963008d1abb465a3
ugojo <obiugochukwu003@gmail.com>
2020-12-16 05:31:14 -0800

    update

--- js/quiz-script.js
+++ js/quiz-script.js
@@ -1,123 +1,119 @@
-let myInput = document.querySelector(".myInput");
 
-function seeTip(){
-     document.querySelector(".col-fact").style.display = "block";
-}
-function colseFact() {
-     document.querySelector(".col-fact").style.display = "none";
-}
-let startBtn = document.querySelector(".startBtn").addEventListener("click" , function() {
-    
-    if (myInput.value === "") {
-   
-       let  startBtn = document.querySelector(".startBtn").innerHTML = "Type your name.."
-    }
-    else {
-        document.querySelector(".col-startQuiz").style.display = "none";
-        document.querySelector(".col-meun").style.display = "block";
-    }
+const myInput = document.querySelector(".myInput");
+const startBtn = document.querySelector(".startBtn").addEventListener("click" , function() {
+
+    document.querySelector(".row.one").style.display = "none";
+    document.querySelector(".row.two").style.display = "block";
+    selfWriter()
 })
-function colTip(){
-     document.querySelector(".col-meun").style.display = "none";
-     document.querySelector(".col-tip").style.display = "block";
-     selfWriter()
+
+function back(){
+    document.querySelector(".row.one").style.display = "block";
+    document.querySelector(".row.two").style.display = "none";
 }
-let facts = [
-     {
-         fact:"EVERYONE IS BUILT TO BE GREAT ACTION DETERMINES THE REST"
-     },
-     {
-        fact:"ONE OF THE MOST EXPENSIVE THINGS YOU CAN DO IS PAY ATTENTION TO THE WRONG PEOPLE"
-    },
-    {
-        fact:"IF YOU WANT TO GET ANYTHING DONE YOU DO IT YOUR SELF"
-    },
-    {
-        fact:"THOSE WHO ARE FOCUSED ON SELF IMPROVEMENT HAVE NO TIME FOR ENVY"
-    },
-    {
-        fact:"LOVE IS NOT IN THE WORD BUT IN THE ACTION AND HEART"
-    },
-    {
-        fact:"IF YOU DONT KNOW WHO YOU ARE YOU BECOME A WEAPON AGAINST YOURSELF"
-    },
-    {
-        fact:"THE HUSBAND MAY BE THE HEAD OF HOME BUT THE WIFE IS THE HEART OF HOME"
-    },
-    {
-        fact:"IGNORING YOUR PASSION IS SLOW SUICIDE"
-    },
-    {
-        fact:"BE HELPFUL WHEN YOU SEE A PRESON WITHOUT A SMILE GIVE THEM YOURS"
-    },
-    {
-        fact:"ELEGANCE IS AND ATTITUDE"
-    },
-    {
-        fact:"SUCCESS IS NOTHING MORE THAN A FEW SIMPLE DISCIPLINES, PRACTIED EVERYDAY"
-    }
-]   
-    let x = 0 ;
-    let  speed = 70 ;
-    let currentFact = facts.sort(function() {
-        return 0.5 - Math.random();
-    })
-    function selfWriter(){
+
+function nextQuest(){
+
+    document.querySelector(".row.two").style.display = "none";
+    document.querySelector(".row.three").style.display = "block";
+}
+
+function currentAffair(){
     
-    if ( 1 < currentFact[0].fact.length) {
-        
-        document.getElementById("demo").innerHTML += currentFact[0].fact.charAt(x);
-        x++
-        setTimeout(selfWriter ,speed);
-    }
-}
-function startQuiz() {
-    document.querySelector(".col-tip").style.display = "none";
-    document.querySelector(".col-main").style.display = "block";
-    document.querySelector(".logo").style.display = "none";
+    document.querySelector(".row.three").style.display = "none";
+    document.querySelector(".row.four").style.display = "block";
     showQuestion()
 }
-/*---------------- highScore -------------------*/
-function showScore(){
-    document.querySelector(".col-recentScore").style.display = "block";
-}
-let closeScore = document.querySelector(".closeRecentScore").onclick = function() {
+function seeTip(){
     
-    document.querySelector(".col-recentScore").style.display = "none";
+    document.querySelector(".tipBox").style.display = "block";
+}
+function closeTip(){
+    document.querySelector(".tipBox").style.display = "none";
+}
+
+let facts = [
+    {
+        fact:"EVERYONE IS BUILT TO BE GREAT ACTION DETERMINES THE REST"
+    },
+    {
+       fact:"ONE OF THE MOST EXPENSIVE THINGS YOU CAN DO IS PAY ATTENTION TO THE WRONG PEOPLE"
+   },
+   {
+       fact:"IF YOU WANT TO GET ANYTHING DONE YOU DO IT YOUR SELF"
+   },
+   {
+       fact:"THOSE WHO ARE FOCUSED ON SELF IMPROVEMENT HAVE NO TIME FOR ENVY"
+   },
+   {
+       fact:"LOVE IS NOT IN THE WORD BUT IN THE ACTION AND HEART"
+   },
+   {
+       fact:"IF YOU DONT KNOW WHO YOU ARE YOU BECOME A WEAPON AGAINST YOURSELF"
+   },
+   {
+       fact:"THE HUSBAND MAY BE THE HEAD OF HOME BUT THE WIFE IS THE HEART OF HOME"
+   },
+   {
+       fact:"IGNORING YOUR PASSION IS SLOW SUICIDE"
+   },
+   {
+       fact:"BE HELPFUL WHEN YOU SEE A PRESON WITHOUT A SMILE GIVE THEM YOURS"
+   },
+   {
+       fact:"ELEGANCE IS AND ATTITUDE"
+   },
+   {
+       fact:"SUCCESS IS NOTHING MORE THAN A FEW SIMPLE DISCIPLINES, PRACTIED EVERYDAY"
+   }
+]   
+   let x = 0 ;
+   let  speed = 70 ;
+   let currentFact = facts.sort(function() {
+       return 0.5 - Math.random();
+   })
+   function selfWriter(){
+   
+   if ( 1 < currentFact[0].fact.length) {
+       
+       document.getElementById("demo").innerHTML += currentFact[0].fact.charAt(x);
+       x++
+       setTimeout(selfWriter ,speed);
+   }
 }
 
 /* Question Part */
 let questions = [
-     {
-        question: 'where is the world oldest University located ',
-        opt1: 'Australia',
-        opt2: 'Europe',
-        opt3: 'Africa',
-        opt4: 'Asia',
+    {
+       question: 'where is the world oldest University located ',
+       opt1: 'Australia',
+       opt2: 'Europe',
+       opt3: 'Africa',
+       opt4: 'Asia',
+       answer: 3,
+       hint: 'The University of AL-Qarawinyyin is the oldest existing,' +
+        'continually operating na dfirst degree-awarding educational instition in the world'
+    },{
+      question: 'How much percent of Elephant do Africa own',
+      opt1: '70 - 85%',
+      opt2: '20  -30%',
+      opt3: '5 - 20%',
+      opt4: '100%',
+      answer: 1 ,
+      hint: 'Afican has over 85% of the world\'s elephants,which can weigh up to 7 tons and over 99% of the' +
+       'remainig liones are on the African continent'
+    },
+    {
+        question: 'The richest Africa man is from which of these county',
+        opt1: 'South africa',
+        opt2: 'Ghana',
+        opt3: 'Nigeria',
+        opt4: 'Algeria',
         answer: 3,
-        hint: 'The University of AL-Qarawinyyin is the oldest existing,' +
-         'continually operating na dfirst degree-awarding educational instition in the world'
-     },{
-       question: 'How much percent of Elephant do Africa own',
-       opt1: '70 - 85%',
-       opt2: '20  -30%',
-       opt3: '5 - 20%',
-       opt4: '100%',
-       answer: 1 ,
-       hint: 'Afican has over 85% of the world\'s elephants,which can weigh up to 7 tons and over 99% of the' +
-        'remainig liones are on the African continent'
-     },
-     {
-         question: 'The richest Africa man is from which of these county',
-         opt1: 'South africa',
-         opt2: 'Ghana',
-         opt3: 'Nigeria',
-         opt4: 'Algeria',
-         answer: 3,
-         hint: 'Aliko Dangote is the richest man in Africa and he is a Nigerian'
-     }
+        hint: 'Aliko Dangote is the richest man in Africa and he is a Nigerian'
+    }
 
-    ]
+   ]
 /*------------- show Question--------------------*/
 let score = 0;
 let worngAws = 0;
@@ -125,27 +121,26 @@ let questionLen = questions.length ;
 let currentIndex = 0 ;
 let showLength ;
 let showingQuestion = document.querySelector(".question");
-    let optionsBtn = Array.from( document.querySelectorAll(".option"));
-    let currentQuestion = [...questions].sort(function() {
-        return 0.5 - Math.random();
-    }) ;
+   let optionsBtn = Array.from( document.querySelectorAll(".option"));
+   let currentQuestion = [...questions].sort(function() {
+       return 0.5 - Math.random();
+   }) ;
 
 let showingNow = 1 ;
 
- function showQuestion() {
+function showQuestion() {
 
-    showingQuestion.innerHTML = currentQuestion[currentIndex].question ;
-    optionsBtn.forEach(option => {
-        
-        let optdata = option.dataset['opt'];
+   showingQuestion.innerHTML = currentQuestion[currentIndex].question ;
+   optionsBtn.forEach(option => {
+       
+       let optdata = option.dataset['opt'];
 
-       option.innerHTML =  currentQuestion[currentIndex]['opt' + optdata];
+      option.innerHTML =  currentQuestion[currentIndex]['opt' + optdata];
 
-       showLength = document.querySelector(".questionNum").innerHTML = showingNow +" " + 'of' + " " + questionLen; 
-    });
+      showLength = document.querySelector(".questionNum").innerHTML = showingNow +" " + 'of' + " " + questionLen; 
+   });
 
 /*------- checking for correct Answer ------------*/
-   
 let correct = false ;
 let accpectingAnws = true ;
   optionsBtn.forEach(option => {
@@ -166,19 +161,17 @@ let accpectingAnws = true ;
        score++ ;
        correct = true;
       currentTarget.className += ' correct';
-      
-      document.querySelector(".score").innerHTML ='socre :' + score;
+    
    }else{
        worngAws++;
        correct = false ;
        currentTarget.className += ' worng';
         
-       document.querySelector(".worngAnw").innerHTML = 'worng :' + worngAws ;
    }
    if (!accpectingAnws) {
 
-       document.querySelector(".col-fact-two p").innerHTML = currentQuestion[currentIndex].hint ;
-       document.querySelector(".col-fact p").innerHTML = currentQuestion[currentIndex].hint ;
+       document.querySelector("div.tipsView p").innerHTML = currentQuestion[currentIndex].hint ;
+       document.querySelector("div .tips").innerHTML = currentQuestion[currentIndex].hint ;
    }
   }
  }
@@ -209,24 +202,20 @@ if ( currentIndex == questions.length) {
         optionsBtn[i].className = " option";
      }
 }
-/*------ Submitting the Question ----------- */
+
 function submitQuestion() {
     
-    document.querySelector(".col-submit").style.display = "block";
-    document.querySelector(".playerName").innerHTML = myInput.value ;
-    document.querySelector(".playerScore").innerHTML = score ;
-    document.querySelector(".worngAns").innerHTML = worngAws ;
+    document.querySelector(".row.four").style.display = "none";
+    document.querySelector(".row.five").style.display = "block";
+    document.querySelector(".showScore").innerHTML = score ;
+    document.querySelector(".number").innerHTML = questionLen ;
     
 }
-/*--------  Store Submited  Score ------------*/
 
- function  storeScore() {
+function  storeScore() {
     
     let playerScores;
-    let recentScore = {
-                      name : myInput.value,
-                      scores : score
-                       }
+    let recentScore = score ;
 
     if ( localStorage.getItem("playerScores") === null) {
         
@@ -239,26 +228,7 @@ function submitQuestion() {
      localStorage.setItem("playerScores", JSON.stringify(playerScores) );
    
  }
- if ( JSON.parse( localStorage.getItem("playerScores") !== null)) {
-        
-    let recentScores = JSON.parse( localStorage.getItem("playerScores"));
-    
-      recentScores.forEach( score => {
-
-      let  newList = document.createElement("li");
-          newList.className = 'myScore' ;
-           newList.innerHTML = score.name;
-       
-       let highScore = document.createElement("li")
-           highScore.className = 'highscore' 
-           highScore.innerHTML = score.scores;
-             
-      document.querySelector(".score-box p").appendChild(newList)
-      document.querySelector(".score-box p").appendChild(highScore);
-        
-
-    });
-   }
+ 
    function clearScore() {
        
     localStorage.removeItem("playerScores")
